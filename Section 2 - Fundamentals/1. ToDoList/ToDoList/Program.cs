53c85a5f28ba154c59c15ab2a387c25c91facadc
Couvs <couvs81@gmail.com>
2026-05-14 21:38:01 -0400

    start of section 2 project

---
+++ Section 2 - Fundamentals/1. ToDoList/ToDoList/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ToDoList
+{
+    internal class Program
+    {
+        static void ShowMenu()
+        {
+            Console.WriteLine("What do you want to do?\n" 
+                            + "[S]ee all TODOs\n"
+                            + "[A]dd a TODO\n"
+                            + "[R]emove a TODO\n"
+                            + "[Exit]");
+        }
+
+        static void PrintToDoList(List<string> list)
+        {
+
+
+        }
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            ConsoleKeyInfo userChoice;
+            do
+            {
+                ShowMenu();
+                userChoice = Console.ReadKey(true);
+                Console.WriteLine(userChoice.KeyChar);
+
+            } while (userChoice.KeyChar != 'E' && userChoice.KeyChar != 'e');
+
+            
+            
+        }
+    }
+}
